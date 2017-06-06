62191d5c120288d349fcd47d24f6014d9c7b2e80
Chunsheng Yu <cyu.kore@gmail.com>
2017-06-06 11:14:42 -0500

    Add pages

--- face_api_wpf/MainWindow.xaml.cs
+++ face_api_wpf/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Configuration;
 
 using Microsoft.ProjectOxford.Face;
 using Microsoft.ProjectOxford.Face.Contract;
-
+using face_api_wpf.Views;
 
 namespace face_api_wpf
 {
@@ -24,90 +24,14 @@ namespace face_api_wpf
             var appSettings = ConfigurationManager.AppSettings;
             string result = appSettings["test"] ?? "Not Found";
             Title = String.Format("App Setting {0}", result);
+            _scenarioFrame.NavigationService.Navigate(new LoginPage());
         }
 
         private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("36d7fbe10dfc46e7887edde19be2214b",
             "https://westcentralus.api.cognitive.microsoft.com/face/v1.0");
 
-        private async void BrowseButton_Click(object sender, RoutedEventArgs e)
-        {
-            var openDlg = new Microsoft.Win32.OpenFileDialog();
+       
 
-            openDlg.Filter = "JPEG Image(*.jpg)|*.jpg";
-            bool? result = openDlg.ShowDialog(this);
-
-            if (!(bool)result)
-            {
-                return;
-            }
-
-            string filePath = openDlg.FileName;
-
-            Uri fileUri = new Uri(filePath);
-            BitmapImage bitmapSource = new BitmapImage();
-
-            bitmapSource.BeginInit();
-            bitmapSource.CacheOption = BitmapCacheOption.None;
-            bitmapSource.UriSource = fileUri;
-            bitmapSource.EndInit();
-
-            FacePhoto.Source = bitmapSource;
-
-            Title = "Detecting...";
-            FaceRectangle[] faceRects = await UploadAndDetectFaces(filePath);
-            Title = String.Format("Detection Finished. {0} face(s) detected", faceRects.Length);
-
-            if (faceRects.Length > 0)
-            {
-                DrawingVisual visual = new DrawingVisual();
-                DrawingContext drawingContext = visual.RenderOpen();
-                drawingContext.DrawImage(bitmapSource,
-                    new Rect(0, 0, bitmapSource.Width, bitmapSource.Height));
-                double dpi = bitmapSource.DpiX;
-                double resizeFactor = 96 / dpi;
-
-                foreach (var faceRect in faceRects)
-                {
-                    drawingContext.DrawRectangle(
-                        Brushes.Transparent,
-                        new Pen(Brushes.Red, 2),
-                        new Rect(
-                            faceRect.Left * resizeFactor,
-                            faceRect.Top * resizeFactor,
-                            faceRect.Width * resizeFactor,
-                            faceRect.Height * resizeFactor
-                            )
-                    );
-                }
-
-                drawingContext.Close();
-                RenderTargetBitmap faceWithRectBitmap = new RenderTargetBitmap(
-                    (int)(bitmapSource.PixelWidth * resizeFactor),
-                    (int)(bitmapSource.PixelHeight * resizeFactor),
-                    96,
-                    96,
-                    PixelFormats.Pbgra32);
-
-                faceWithRectBitmap.Render(visual);
-                FacePhoto.Source = faceWithRectBitmap;
-            }
-        }
-
-        private async Task<FaceRectangle[]> UploadAndDetectFaces(string imageFilePath)
-        {
-            try
-            {
-                using (Stream imageFileStream = File.OpenRead(imageFilePath))
-                {
-                    var faces = await faceServiceClient.DetectAsync(imageFileStream);
-                    var faceRects = faces.Select(face => face.FaceRectangle);
-                    return faceRects.ToArray();
-                }
-            }
-            catch (Exception)
-            {
-                return new FaceRectangle[0];
-            }
-        }
+        
     }
 }
